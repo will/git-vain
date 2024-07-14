cafe53bf226b77b5c2170b0b7b3dfb361a4575a5
Will Leinweber <will@bitfission.com>
2024-07-14 18:05:26 +0200

    target

--- src/main.zig
+++ src/main.zig
@@ -19,10 +19,11 @@ var GlobalFoundFlag = FoundFlag{};
 
 pub fn main() !void {
     var sha = GitSha.init();
+    const target = try Target.init("cafe");
 
     const thread_count = Cpu.getPerfCores();
     for (0..thread_count) |i| {
-        const handle = try std.Thread.spawn(.{}, search, .{ i, thread_count, &sha });
+        const handle = try std.Thread.spawn(.{}, search, .{ i, thread_count, &sha, target });
         handle.detach();
     }
 
@@ -33,22 +34,20 @@ pub fn main() !void {
     print("All your {s} are belong to {x}.\n", .{ "stuff", final });
 }
 
-fn search(start: u64, step: u8, sha: *GitSha) !void {
+fn search(start: u64, step: u8, sha: *GitSha, target: Target) !void {
     var i = start;
 
-    while (true) {
+    while (true) : (i += step) {
         if (GlobalFoundFlag.found) break;
 
         var buf: [20]u8 = undefined;
         const numAsString = try std.fmt.bufPrint(&buf, "{}", .{i});
         const result = sha.trySha(numAsString);
-        if (result[0] == 0 and result[1] == 0 and result[2] == 0 and result[3] != 0) {
+        if (target.match(&result)) {
             if (GlobalFoundFlag.setFound()) {
                 print("{d}: {x}\n", .{ i, result });
             }
             break;
         }
-
-        i += step;
     }
 }
--- src/target.zig
+++ src/target.zig
@@ -4,15 +4,112 @@ const Self = @This();
 
 buf: [20]u8 = undefined,
 buf_len: u8 = 0,
+half: bool = false,
 
-pub fn init(str: []const u8) Self {
-    _ = str;
-    return Self{};
+const TargetError = error{
+    // not 0-9 or a-z
+    NotHexChr,
+    NoInput,
+    TooLong,
+};
+
+pub fn init(str: []const u8) TargetError!Self {
+    if (str.len < 1) return TargetError.NoInput;
+    if (str.len > 16) return TargetError.TooLong; // NOTE: just picking this, real hard limit would probably be 40?
+
+    var half = false;
+    var buf: [20]u8 = undefined;
+    var i: u8 = 0;
+
+    var itr = std.mem.window(u8, str, 2, 2);
+    while (itr.next()) |ch| : (i += 1) {
+        var val = try hexChrToInt(ch[0]);
+        val <<= 4;
+
+        if (ch.len == 2) {
+            val += try hexChrToInt(ch[1]);
+        } else {
+            half = true;
+        }
+
+        buf[i] = val;
+    }
+
+    return Self{ .buf = buf, .buf_len = i, .half = half };
+}
+
+fn hexChrToInt(chr: u8) TargetError!u8 {
+    if (chr >= '0' and chr <= '9') {
+        return chr - '0';
+    } else if (chr >= 'a' and chr <= 'f') {
+        return chr + 10 - 'a';
+    } else return TargetError.NotHexChr;
 }
 
 test "init" {
-    const t = Self.init("cafe");
-    _ = t;
+    var t = try Self.init("cafe12");
+    try std.testing.expectEqual(3, t.buf_len);
+    try std.testing.expectEqualDeep(([3]u8{ 0xca, 0xfe, 0x12 })[0..], t.buf[0..3]);
+    try std.testing.expectEqual(false, t.half);
 
-    try std.testing.expect(true);
+    t = try Self.init("123");
+    try std.testing.expectEqual(t.buf_len, 2);
+    try std.testing.expectEqualDeep(t.buf[0..2], ([2]u8{ 0x12, 0x30 })[0..]);
+    try std.testing.expectEqual(true, t.half);
+
+    t = try Self.init("abcdef0123456789");
+    try std.testing.expectEqual(8, t.buf_len);
+    try std.testing.expectEqual(false, t.half);
+
+    try std.testing.expectError(TargetError.NotHexChr, Self.init("great"));
+    try std.testing.expectError(TargetError.NoInput, Self.init(""));
+    try std.testing.expectError(TargetError.TooLong, Self.init("12345678901234567"));
+}
+
+pub fn match(self: *const Self, other: *const [20]u8) bool {
+    const stop = self.buf_len - 1;
+
+    for (0..stop) |i| {
+        if (self.buf[i] != other[i]) return false;
+    }
+
+    if (self.half) {
+        if (self.buf[stop] >> 4 != other[stop] >> 4) return false;
+    } else {
+        if (self.buf[stop] != other[stop]) return false;
+    }
+
+    return true;
+}
+
+test "match" {
+    var sha = @import("gitSha.zig").init();
+    const result = sha.trySha("howdy"); // { ef, 42, ba, b1, 19, 1d, a2, 72, f1, 39, 35, f7, 8c, 40, 1e, 3d, e0, c1, 1a, fb }
+
+    var t = try Self.init("cafe12");
+    try std.testing.expectEqual(false, t.match(&result));
+
+    // even number of chrs
+    t = try Self.init("ef");
+    try std.testing.expectEqual(true, t.match(&result));
+    t = try Self.init("ef42bab1");
+    try std.testing.expectEqual(true, t.match(&result));
+    t = try Self.init("ef40bab1"); // error in middle
+    try std.testing.expectEqual(false, t.match(&result));
+    t = try Self.init("ef42bab100"); // make sure last chrs are actually checked
+    try std.testing.expectEqual(false, t.match(&result));
+
+    // odd
+    t = try Self.init("e");
+    try std.testing.expectEqual(true, t.match(&result));
+    t = try Self.init("f");
+    try std.testing.expectEqual(false, t.match(&result));
+    t = try Self.init("ef42b");
+    try std.testing.expectEqual(true, t.match(&result));
+    t = try Self.init("ef42a"); // error in last chr
+    try std.testing.expectEqual(false, t.match(&result));
+    t = try Self.init("ef42bab1191da");
+    try std.testing.expectEqual(true, t.match(&result));
+    t = try Self.init("ef02bab1191da"); // error in 3rd chr
+    try std.testing.expectEqual(false, t.match(&result));
 }
